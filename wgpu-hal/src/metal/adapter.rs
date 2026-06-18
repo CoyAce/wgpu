2d2af54c2f17e2a64452bd5824f1320b42acf8b0
CoyAce <akeycoy@gmail.com>
2026-06-18 22:31:48 +0800

    modify msl version

--- wgpu-hal/src/metal/adapter.rs
+++ wgpu-hal/src/metal/adapter.rs
@@ -651,11 +651,13 @@ impl super::CapabilitiesQuery {
                     // Mesh shaders don't work on virtual devices even if they should be supported. CI thing
                 && !is_virtual;
 
-        let msl_version = if available!(macos = 15.0, ios = 18.0, tvos = 18.0, visionos = 2.0) {
+        let metal3_or_metal4 = metal3 || metal4;
+
+        let msl_version = if metal3_or_metal4 && available!(macos = 15.0, ios = 18.0, tvos = 18.0, visionos = 2.0) {
             MTLLanguageVersion::Version3_2
-        } else if available!(macos = 14.0, ios = 17.0, tvos = 17.0, visionos = 1.0) {
+        } else if metal3_or_metal4 && available!(macos = 14.0, ios = 17.0, tvos = 17.0, visionos = 1.0) {
             MTLLanguageVersion::Version3_1
-        } else if available!(macos = 13.0, ios = 16.0, tvos = 16.0, visionos = 1.0) {
+        } else if metal3_or_metal4 && available!(macos = 13.0, ios = 16.0, tvos = 16.0, visionos = 1.0) {
             MTLLanguageVersion::Version3_0
         } else if available!(macos = 12.0, ios = 15.0, tvos = 15.0, visionos = 1.0) {
             MTLLanguageVersion::Version2_4
